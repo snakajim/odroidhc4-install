01880335609deb5963988b3e550a1c2841d12c73
snakajim <70503324+snakajim@users.noreply.github.com>
2021-01-16 21:41:28 +0900

    Update install_llvm.sh

--- scripts/install_llvm.sh
+++ scripts/install_llvm.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
+#
+# This script is tested on Aarch64 Ubuntu20.04 LTS only. 
 
-cd ${HOME}/tmp && rm -rf *
+export CXX="/usr/bin/g++-7"
+export CC="/usr/bin/gcc-7"
 
+cd ${HOME}/tmp && rm -rf llvm*
 
 # ---------------------------
 # Confirm which OS you are in 
@@ -20,37 +24,37 @@ else
 fi
 
 #
-# install LLVM 1100
+# install LLVM 1101
 #
 cd ${HOME}/tmp && rm -rf *
-cd ${HOME}/tmp && aria2c -x10 https://github.com/llvm/llvm-project/releases/download/llvmorg-11.0.0/llvm-project-11.0.0.tar.xz
-unxz llvm-project-11.0.0.tar.xz && tar xvf llvm-project-11.0.0.tar && \
-  cd llvm-project-11.0.0 && mkdir -p build && cd build
+cd ${HOME}/tmp && aria2c -x10 https://github.com/llvm/llvm-project/releases/download/llvmorg-11.0.1/llvm-project-11.0.1.src.tar.xz
+unxz llvm-project-11.0.1.src.tar.xz && tar xvf llvm-project-11.0.1.src.tar && \
+  cd llvm-project-11.0.1.src && mkdir -p build && cd build
 if [ $OSNOW = "UBUNTU" ]; then 
   cmake -G Ninja -G "Unix Makefiles"\
-    -DCMAKE_C_COMPILER="/usr/bin/gcc" \
-    -DCMAKE_CXX_COMPILER="/usr/bin/g++"\
+    -DCMAKE_C_COMPILER=$CC \
+    -DCMAKE_CXX_COMPILER=$CXX \
     -DLLVM_ENABLE_PROJECTS=clang \
     -DCMAKE_BUILD_TYPE=RELEASE \
     -DLLVM_TARGETS_TO_BUILD="ARM;AArch64"\
-    -DCMAKE_INSTALL_PREFIX="/usr/local/llvm_1100" \
-    ../llvm && make -j12 && sudo make install
+    -DCMAKE_INSTALL_PREFIX="/usr/local/llvm_1101" \
+    ../llvm && make -j4 && sudo make install
 elif [ $OSNOW = "CENTOS" ]; then
   cmake -G Ninja -G "Unix Makefiles"\
-    -DCMAKE_C_COMPILER="/usr/local/bin/gcc" \
-    -DCMAKE_CXX_COMPILER="/usr/local/bin/g++"\
+    -DCMAKE_C_COMPILER=$CC \
+    -DCMAKE_CXX_COMPILER=$CXX \
     -DLLVM_ENABLE_PROJECTS=clang \
     -DCMAKE_BUILD_TYPE=RELEASE \
     -DLLVM_TARGETS_TO_BUILD="ARM;AArch64"\
-    -DCMAKE_INSTALL_PREFIX="/usr/local/llvm_1100" \
-    ../llvm && make -j12 && sudo make install
+    -DCMAKE_INSTALL_PREFIX="/usr/local/llvm_1101" \
+    ../llvm && make -j4 && sudo make install
 else
   echo "please set right choise in OS=$OSNOW.."
 fi
 make clean
 echo "# " >> ${HOME}/.bashrc
 echo "# LLVM setting" >> ${HOME}/.bashrc
-echo "export LLVM_DIR=/usr/local/llvm_1100">> ${HOME}/.bashrc
+echo "export LLVM_DIR=/usr/local/llvm_1101">> ${HOME}/.bashrc
 echo "export PATH=\$LLVM_DIR/bin:\$PATH" >>  ${HOME}/.bashrc
 
 exec $SHELL -l
