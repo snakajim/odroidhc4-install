1cf0fdabd3b56f830855d1eee42f1c84a0b30fb2
snakajim <70503324+snakajim@users.noreply.github.com>
2021-01-16 17:49:44 +0900

    Update install_acl.sh

--- scripts/install_acl.sh
+++ scripts/install_acl.sh
@@ -6,14 +6,16 @@
 # Downlaod and install
 #
 cd ${HOME}/tmp
-git clone https://github.com/ARM-software/ComputeLibrary.git -b v20.11
+if [ ! -d ComputeLibrary ]; then
+  git clone https://github.com/ARM-software/ComputeLibrary.git -b v20.11
+fi
 cd ${HOME}/tmp/ComputeLibrary
 
 # Get Force scon using clang instead of gcc. Make sure you have llvm by either
 # - running install_llvm.sh
 # - apt-get clang
 #
-if [-d /usr/local/llvm_1100/bin ]; then
+if [ -d /usr/local/llvm_1100/bin ]; then
   export CXX="/usr/local/llvm_1100/bin/clang++"
   export CC="/usr/local/llvm_1100/bin/clang"
   echo "setting CXX as ${CXX}"
@@ -23,7 +25,13 @@ else
   echo "setting CXX as ${CXX}"
 fi
 
+# patch in Sconstruct
+# Original
+#
+# Modified
+#
+
 # install Arm Compute Library v20.11
 #
 #
-#scons Werror=1 debug=0 asserts=0 arch=arm64-v8.2-a neon=1 opencl=1 examples=1 build=native pmu=1 benchmark_tests=1 -j4
+#scons Werror=1 debug=0 asserts=0 arch=arm64-v8.2-a os=linux neon=1 opencl=1 examples=1 build=native pmu=1 benchmark_tests=1 -j4
