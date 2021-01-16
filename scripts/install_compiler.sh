f768344db4c38d0ec4326e5eb901c727597b9cc0
snakajim <70503324+snakajim@users.noreply.github.com>
2021-01-16 21:40:48 +0900

    Update install_compiler.sh

--- scripts/install_compiler.sh
+++ scripts/install_compiler.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
 #
-# TBD, aarch64 native compiler
+# GNU Arm Embedded Toolchain Version 10-2020-q4-major 
 #
+cd ${HOME}/tmp && \
+aria2c -x6 https://developer.arm.com/-/media/Files/downloads/gnu-rm/10-2020q4/gcc-arm-none-eabi-10-2020-q4-major-aarch64-linux.tar.bz2
+sudo sh -c "cd /usr/local && tar jvxf ${HOME}/tmp/gcc-arm-none-eabi-10-2020-q4-major-aarch64-linux.tar.bz2"
+
+#
+# set path
+#
+cd ${HOME} && \
+  echo "export PATH=\$PATH:/usr/local/gcc-arm-none-eabi-10-2020-q4-major/bin" >> .bashrc
