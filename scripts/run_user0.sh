9bf2c99046816a65170a7a3c765b4df14c00dc94
snakajim <satoshi.nakajima@arm.com>
2021-01-20 09:05:12 +0900

    user0 bach run

---
+++ scripts/run_user0.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#
+# batch for install_xx.sh
+# 
+# How to run:
+# $> source ./run_user0.sh
+#
+
+# ACL with gcc
+#\time -ao out.log echo "ahoge" >& out.log
+\time -ao ./install_acl.gcc.log ./install_acl.sh >& ./install_acl.gcc.log
+
+# install LLVM1101
+\time -ao ./install_llvm.log ./install_llvm.sh >& ./install_llvm.log 
+exec $SHELL -l
+
+# ACL with LLVM
+\time -ao ./install_acl.llvm.log ./install_acl.sh >& ./install_acl.llvm.log 
+ 
