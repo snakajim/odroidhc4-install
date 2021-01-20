2ad345c35cad4ef82f026251c5626ceb8fe2ad67
snakajim <satoshi.nakajima@arm.com>
2021-01-20 13:04:05 +0900

    to let use at command

--- scripts/run_user0.sh
+++ scripts/run_user0.sh
@@ -7,15 +7,15 @@
 #
 
 # ACL with gcc
-#\time -ao out.log echo "ahoge" >& out.log
+#/usr/bin/time -ao out.log echo "ahoge" >& out.log
 unset CC
 unset CXX
-\time -ao ./install_acl.gcc.log ./install_acl.sh >& ./install_acl.gcc.log
+/usr/bin/time -ao ./install_acl.gcc.log ./install_acl.sh >& ./install_acl.gcc.log
 
 # install LLVM1101
-\time -ao ./install_llvm.log ./install_llvm.sh >& ./install_llvm.log 
+/usr/bin/time -ao ./install_llvm.log ./install_llvm.sh >& ./install_llvm.log 
 exec $SHELL -l
 
 # ACL with LLVM
-\time -ao ./install_acl.llvm.log ./install_acl.sh >& ./install_acl.llvm.log 
+/usr/bin/time -ao ./install_acl.llvm.log ./install_acl.sh >& ./install_acl.llvm.log 
  
