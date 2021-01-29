ff5bae529e9d827d2ee29e8d7972d6769498d2d6
snakajim <satoshi.nakajima@arm.com>
2021-01-29 19:10:18 +0900

    adding sudo install

--- scripts/install_basic.sh
+++ scripts/install_basic.sh
@@ -7,7 +7,7 @@
 # install several tools by apt-get
 #
 apt-get install -y default-jre default-jdk
-apt-get install -y curl cmake ninja-build z3
+apt-get install -y curl cmake ninja-build z3 sudo
 apt-get install -y autoconf flex bison apt-utils
 apt-get install -y python3 python3-dev python3-pip
 apt-get install -y openssh-server x11-apps at
