255550e7546c0fee41af937b30ea2c14f6744c40
snakajim <satoshi.nakajima@arm.com>
2021-01-31 09:13:50 +0900

    to add linaro tool chain

--- scripts/install_gmp_mprf.sh
+++ scripts/install_gmp_mprf.sh
@@ -12,17 +12,17 @@ aria2c -x6 ftp://gcc.gnu.org/pub/gcc/infrastructure/mpfr-3.1.4.tar.bz2
 aria2c -x6 ftp://gcc.gnu.org/pub/gcc/infrastructure/mpc-1.0.3.tar.gz
 
 # install GMP 6.1.0
-tar xvf gmp-6.1.0.tar.bz2 && cd gmp-6.1.0 
-mkdir -p build && cd build
+tar xvf ${HOME}/tmp/linaro/gmp-6.1.0.tar.bz2 && cd ${HOME}/tmp/linaro/gmp-6.1.0 
+mkdir -p ${HOME}/tmp/linaro/gmp-6.1.0/build && cd ${HOME}/tmp/linaro/gmp-6.1.0/build
 ../configure --prefix=/usr/local/linaro && make -j4 && make check && sudo make install
 
 
 # install MPRF 3.1.4
 tar xvf mpfr-3.1.4.tar.bz2 && cd mpfr-3.1.4 
-mkdir -p build && cd build
-../configure && make -j4 && make check && sudo make install
+mkdir -p ${HOME}/tmp/linaro/mpfr-3.1.4/build && cd ${HOME}/tmp/linaro/mpfr-3.1.4/build
+../configure --prefix=/usr/local/linaro && make -j4 && make check && sudo make install
 
 # install MPC 1.0.3
 tar xvf mpc-1.0.3.tar.gz && cd mpc-1.0.3 
-mkdir -p build && cd build
-../configure && make -j4 && make check && sudo make install
+mkdir -p ${HOME}/tmp/linaro/mpc-1.0.3/build && cd ${HOME}/tmp/linaro/mpc-1.0.3/build
+../configure --prefix=/usr/local/linaro && make -j4 && make check && sudo make install
